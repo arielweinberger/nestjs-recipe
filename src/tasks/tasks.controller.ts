32b9e5324dc97c39837b93c2f5a26649e5d9a1d0
Ariel Weinberger <weinberger.ariel@gmail.com>
2019-05-12 20:32:59 +0200

    Create tasks controller

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,5 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('tasks')
+export class TasksController {
+}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { TasksController } from './tasks.controller';
 
-@Module({})
+@Module({
+  controllers: [TasksController]
+})
 export class TasksModule {}
