1bf1a2fa24cc3bbc99c254fdb6a9ce0ddf81cf94
Ariel Weinberger <weinberger.ariel@gmail.com>
2019-05-14 21:29:49 +0200

    Add createTask() method to the service, install uuid package

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import * as uuid from 'uuid/v1';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +8,17 @@ export class TasksService {
 
   async getAllTasks(): Promise<Task[]> {
     return this.tasks;
+  }
+
+  async createTask(title: string, description: string): Promise<Task> {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
   }
 }
