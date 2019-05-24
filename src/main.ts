d32a8b6e1b647e31d076833bc8cccbc6467e0a16
Ariel Weinberger <weinberger.ariel@gmail.com>
2019-05-24 20:09:13 +0200

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
