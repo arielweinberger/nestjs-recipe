9246c43f3744f473793b78a428a6eefc867dc686
Ariel Weinberger <weinberger.ariel@gmail.com>
2019-05-26 19:24:37 +0200

    Connecting to a Postgres database using TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    TasksModule,
+  ],
 })
 export class AppModule {}
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'taskmanagement',
+  entities: [__dirname + '/../**/*.entity.ts'],
+  synchronize: true,
+};
