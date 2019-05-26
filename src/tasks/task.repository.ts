89e867fe4ccd2e2615c9850cd1efba34446bf778
Ariel Weinberger <weinberger.ariel@gmail.com>
2019-05-26 20:45:19 +0200

    Define task repository and include in module

---
+++ src/tasks/task.repository.ts
@@ -0,0 +1,7 @@
+import { Task } from './task.entity';
+import { EntityRepository, Repository } from 'typeorm';
+
+@EntityRepository(Task)
+export class TaskRepository extends Repository<Task> {
+
+}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskRepository } from './task.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([TaskRepository]),
+  ],
   controllers: [TasksController],
-  providers: [TasksService]
+  providers: [TasksService],
 })
 export class TasksModule {}
