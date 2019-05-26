0c8ea1fd12fe581cbafb970ce1a49bf0bf381e5e
Ariel Weinberger <weinberger.ariel@gmail.com>
2019-05-26 20:39:07 +0200

    Add BaseEntity

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,8 +1,8 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 import { TaskStatus } from './task.model';
 
 @Entity()
-export class Task {
+export class Task extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
