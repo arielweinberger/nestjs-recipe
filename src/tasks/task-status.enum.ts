11918a3ff63965d870c4548fefed89a12a70b11f
Ariel Weinberger <weinberger.ariel@gmail.com>
2019-05-26 19:41:10 +0200

    Apply preparation for data persistence

---
+++ src/tasks/task-status.enum.ts
@@ -0,0 +1,5 @@
+export enum TaskStatus {
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,5 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
-import { TaskStatus } from './task.model';
+import { TaskStatus } from './task-status.enum';
 
 @Entity()
 export class Task {
