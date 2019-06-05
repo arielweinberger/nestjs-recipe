81b347baf0ab49809493a74bb0dfc4ac33214b19
Ariel Weinberger <weinberger.ariel@gmail.com>
2019-06-05 12:45:19 +0200

    Custom GetUser decorator

---
+++ src/auth/get-user.decorator.ts
@@ -0,0 +1,6 @@
+import { createParamDecorator } from '@nestjs/common';
+import { User } from './user.entity';
+
+export const GetUser = createParamDecorator((data, req): User => {
+  return req.user;
+});
