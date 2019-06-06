d05bd5aed210c8f3b0be5fb7dd924365c7fc39d6
Ariel Weinberger <weinberger.ariel@gmail.com>
2019-06-06 22:14:07 +0200

    Applying configuration part

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+
+  const serverConfig = config.get('server');
+  console.log(serverConfig);
+
+  console.log(config.get('db'));
 
   const port = 3000;
   await app.listen(port);
