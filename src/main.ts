5c424ea63fdb3dcd2bc88b31f32189f681264d78
Ariel Weinberger <weinberger.ariel@gmail.com>
2019-06-06 23:33:07 +0200

    Enable CORS in Development Mode

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
   const serverConfig = config.get('server');
 
   const port = process.env.PORT || serverConfig.port;
