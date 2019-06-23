83bd30d8c2785f690a4671583ab470efc74fd53c
Ariel Weinberger <weinberger.ariel@gmail.com>
2019-06-23 18:36:27 +0200

    Revert commit ahead

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(typeOrmConfig),
-    TasksModule,
-  ],
+  imports: [TasksModule],
 })
 export class AppModule {}
