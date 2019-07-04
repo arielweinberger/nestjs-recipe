ca596fe5c0e411605918410449bda58e24bd90c8
Ariel Weinberger <weinberger.ariel@gmail.com>
2019-07-04 22:14:16 +0200

    Add tests for UserEntity

---
+++ src/auth/user.entity.spec.ts
@@ -0,0 +1,31 @@
+import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
+
+describe('User entity', () => {
+  let user: User;
+
+  beforeEach(() => {
+    user = new User();
+    user.password = 'testPassword';
+    user.salt = 'testSalt';
+    bcrypt.hash = jest.fn();
+  });
+
+  describe('validatePassword', () => {
+    it('returns true as password is valid', async () => {
+      bcrypt.hash.mockReturnValue('testPassword');
+      expect(bcrypt.hash).not.toHaveBeenCalled();
+      const result = await user.validatePassword('123456');
+      expect(bcrypt.hash).toHaveBeenCalledWith('123456', 'testSalt');
+      expect(result).toEqual(true);
+    });
+
+    it('returns false as password is invalid', async () => {
+      bcrypt.hash.mockReturnValue('wrongPassword');
+      expect(bcrypt.hash).not.toHaveBeenCalled();
+      const result = await user.validatePassword('wrongPassword');
+      expect(bcrypt.hash).toHaveBeenCalledWith('wrongPassword', 'testSalt');
+      expect(result).toEqual(false);
+    });
+  });
+});
