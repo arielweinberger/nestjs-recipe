d0103f37fc893c4e3060a2be5f4949ece3192ba3
Allan Lykke Christensen <allan.lykke.christensen@gmail.com>
2019-07-07 19:55:56 +0200

    Refactored enum validation

--- src/tasks/pipes/task-status-validation.pipe.ts
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -1,25 +1,13 @@
+import { TaskStatus } from './../task.model';
 import { PipeTransform, BadRequestException } from '@nestjs/common';
-import { TaskStatus } from '../task-status.enum';
 
 export class TaskStatusValidationPipe implements PipeTransform {
-  readonly allowedStatuses = [
-    TaskStatus.OPEN,
-    TaskStatus.IN_PROGRESS,
-    TaskStatus.DONE,
-  ];
-
   transform(value: any) {
-    value = value.toUpperCase();
 
-    if (!this.isStatusValid(value)) {
+    if (!Object.values(TaskStatus).includes(value)) {
       throw new BadRequestException(`"${value}" is an invalid status`);
     }
 
     return value;
-  }
-
-  private isStatusValid(status: any) {
-    const idx = this.allowedStatuses.indexOf(status);
-    return idx !== -1;
   }
 }
