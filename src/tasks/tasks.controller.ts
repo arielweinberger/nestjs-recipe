ec9f6c7dc3e993d7df17b98f16659f480bc33cd0
Ariel Weinberger <weinberger.ariel@gmail.com>
2021-03-13 16:08:38 +0100

    Generate and inject TasksService

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 
 @Module({
   controllers: [TasksController],
+  providers: [TasksService],
 })
 export class TasksModule {}
