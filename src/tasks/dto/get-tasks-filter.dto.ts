20d1356b7675c680dc64641e81e5439640b33634
Ariel Weinberger <weinberger.ariel@gmail.com>
2021-03-14 18:29:35 +0100

    Challenge + Solution: Validation for getTasksWithFilters

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
