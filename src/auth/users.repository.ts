8b748b30506fbded39ea778cc207b7cdb7010144
Ariel Weinberger <weinberger.ariel@gmail.com>
2021-05-08 17:15:29 +0200

    Hash using bcrypt and store secure passwords

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,12 +5,17 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
