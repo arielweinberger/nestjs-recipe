2d9b5336809b45feab229ca8bc368eb8ff1ab02c
Ariel Weinberger <weinberger.ariel@gmail.com>
2021-05-23 15:29:19 +0200

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   const port = 3000;
