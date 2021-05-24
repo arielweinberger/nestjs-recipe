a749b9aa31ec3dc82dbef676c97994ebe34c73be
Ariel Weinberger <weinberger.ariel@gmail.com>
2021-05-24 15:56:57 +0200

    Testing TasksService.getTasks

---
+++ src/tasks/tasks.service.spec.ts
@@ -0,0 +1,39 @@
+import { Test } from '@nestjs/testing';
+import { TasksRepository } from './tasks.repository';
+import { TasksService } from './tasks.service';
+
+const mockTasksRepository = () => ({
+  getTasks: jest.fn(),
+});
+
+const mockUser = {
+  username: 'Ariel',
+  id: 'someId',
+  password: 'somePassword',
+  tasks: [],
+};
+
+describe('TasksService', () => {
+  let tasksService: TasksService;
+  let tasksRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        { provide: TasksRepository, useFactory: mockTasksRepository },
+      ],
+    }).compile();
+
+    tasksService = module.get(TasksService);
+    tasksRepository = module.get(TasksRepository);
+  });
+
+  describe('getTasks', () => {
+    it('calls TasksRepository.getTasks and returns the result', async () => {
+      tasksRepository.getTasks.mockResolvedValue('someValue');
+      const result = await tasksService.getTasks(null, mockUser);
+      expect(result).toEqual('someValue');
+    });
+  });
+});
